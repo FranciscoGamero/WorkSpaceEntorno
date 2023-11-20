028d2e3605c7d264dc87c15c920a4c0de24bc406
Francisco Manuel Gamero Rodríguez <gamero.rofra24@triana.salesianos.edu>
2023-11-20 13:27:05 +0100

    Saludo a los alumnos de ED

---
+++ SaludoED/src/holamundo/SaludoEntornosDesarrollo.java
@@ -0,0 +1,10 @@
+package holamundo;
+
+public class SaludoEntornosDesarrollo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola a todos alumnos de Entornos de Desarrollo");
+	}
+
+}
