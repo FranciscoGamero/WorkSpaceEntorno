3411faa72d5ade1427fbeab2c7fecf6c2ae1b1d7
Francisco Manuel Gamero Rodríguez <gamero.rofra24@triana.salesianos.edu>
2023-11-21 14:05:13 +0100

    Se mejora el saludo

--- SaludoED/src/holamundo/SaludoEntornosDesarrollo.java
+++ SaludoED/src/holamundo/SaludoEntornosDesarrollo.java
@@ -4,7 +4,10 @@ public class SaludoEntornosDesarrollo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.print("-".repeat(50));
 		System.out.println("Hola a todos alumnos de Entornos de Desarrollo");
+		System.out.print("-".repeat(50));
+
 	}
 
 }
